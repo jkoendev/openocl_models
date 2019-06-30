9d61857120a8ddc8c4b1a3789298c3485c8004ae
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-06-30 14:57:01 +0200

    ocl optimmizat initialization mass matrix inv

--- rtb_p560_symbolic_sim/p560_ocl_optimize.m
+++ rtb_p560_symbolic_sim/p560_ocl_optimize.m
@@ -6,12 +6,20 @@ q0 = p560.qn;
 
 solver = ocl.Solver(1, @p560_ocl_vars, @p560_ocl_dae, @p560_ocl_cost);
 
-solver.setInitialBounds('q', q0);
-solver.setInitialBounds('qd', zeros(6,1));
+% solver.setInitialBounds('q', q0);
+% solver.setInitialBounds('qd', zeros(6,1));
 
-solver.setBounds('tau', 0,0)
+solver.setBounds('tau', 0,0);
 
-[sol,times] = solver.solve(solver.ig());
+mass0 = p560_mass_generated(q0,zeros(1,6));
+
+
+ig = solver.ig();
+ig.integrator.algvars.mass_inv.set(inv(mass0));
+ig.states.q.set(q0)
+ig.integrator.states.q.set(q0);
+
+[sol,times] = solver.solve(ig);
 
 q_traj = sol.states.q.value.';
 
--- rtb_p560_symbolic_sim/p560_simulate.m
+++ rtb_p560_symbolic_sim/p560_simulate.m
@@ -10,12 +10,16 @@ y0 = [q0, qd0]';
 
 tspan = 0:0.1:1;
 options = odeset('Mass',@p560_mass_fcn);
-[t, y] = ode15s(@p560_ode_fcn, tspan, y0, options);
+[t_ode15, y_ode15] = ode15s(@p560_ode_fcn, tspan, y0, options);
 
-q = y(:,1:6);
+q_ode15 = y_ode15(:,1:6);
+q_sim = struct;
+q_sim.t = tspan;
+q_sim.q = q_ode15;
+save('q_sim','q_sim');
 
-p560.plot(q)
+p560.plot(q_ode15)
 
 % compare with RTB
-[t q qd] = p560.nofriction.fdyn(1, @(varargin)zeros(1,6), qn);
+[t q qd] = p560.nofriction.fdyn(tspan(2:end), @(varargin)zeros(1,6), qn);
 p560.plot(q)
